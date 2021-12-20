fbcbdbabff344f7db7a4f6127523e5cec8adc765
Fatih20 <fnri39@protonmail.com>
2021-12-20 11:45:34 +0700

    Working header

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
+
+const Main = styled.div`
+    align-items: center;
+    background-color: black;
+    color: white; 
+    display: flex;
+    flex-direction: column;
+    height: 90px;
+    justify-content: center;
+    width: 100%;
+`;
+
+const Title = styled.h1`
+
+`;
+
+export default function Header (){
+    return (
+        <Main>
+            <Title>Calculate your BMI!</Title>
+        </Main>
+    )
+}
