ba4f20c2ef7d606b049ff6c47f5e3a56285ae299
Fatih20 <fnri39@protonmail.com>
2021-12-20 11:45:10 +0700

    Initial work on imperial-metric and calculating BMI

--- src/components/body.js
+++ src/components/body.js
@@ -1,2 +1,33 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import styled from "styled-components";
+
+import Form from "./form";
+import Charts from "./chart";
+
+const Main = styled.div`
+`;
+
+const CalculationContext = React.createContext();
+const ChangeCalculationContext = React.createContext();
+
+export function useCalculationContext (){
+    return useContext(CalculationContext);
+}
+
+export function useChangeCalculationContext(){
+    return useContext(ChangeCalculationContext);
+}
+
+export default function Body (){
+    const[calculation, setCalculation] = useState(null);
+    return (
+        <Main>
+            <CalculationContext.Provider value={calculation}>
+            <ChangeCalculationContext.Provider value={setCalculation}>
+                <Form />
+                <Charts />
+            </ChangeCalculationContext.Provider>
+            </CalculationContext.Provider>
+        </Main>
+    )
+}
--- src/components/form.js
+++ src/components/form.js
@@ -1,2 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+
+import { VanillaButton } from "../GlobalComponent";
+
+import { useCalculationContext, useChangeCalculationContext } from "./body";
+
+const Main = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+`;
+
+const FormBox = styled.div`
+    align-items: center;
+    background-color: #4d4d4d;
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    max-width: 250px;
+    padding: 10px 20px;
+`;
+
+const MetricChoiceContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+`;
+
+const MetricChoice = styled(VanillaButton)`
+    background-color: rgba(0, 0, 0, 0);
+    color: ${({chosen}) => chosen ? "white" : "rgba(255, 255, 255, 0.4)"};
+    border-radius: 5px; 
+`;
+
+
+export default function Form (){
+    const[isMetric, setIsMetric] = useState(true);
+    const calculation = useCalculationContext();
+    const changeCalculation = useChangeCalculationContext();
+
+    function handleMetricChoiceClick (clickedMetric){
+        if (clickedMetric !== isMetric){
+            setIsMetric((prevIsMetric) => !prevIsMetric);
+        }
+    }
+
+    function calculateBMI (height, weight){
+        if (height >= 0 && weight >= 0){
+            let newBMI = height/(weight**2)
+            if (!isMetric){
+                newBMI = newBMI * 703;
+            }
+            changeCalculation(newBMI);
+        }
+    }
+
+    return (
+        <Main>
+            <FormBox>
+                <MetricChoiceContainer>
+                    <MetricChoice chosen={isMetric ? true : false} onClick={() => handleMetricChoiceClick(true)}>Metric</MetricChoice>
+                    <MetricChoice chosen={!isMetric ? true : false}onClick={() => handleMetricChoiceClick(false)}>Imperial</MetricChoice>
+                </MetricChoiceContainer>
+            </FormBox>
+        </Main>
+    )
+}
