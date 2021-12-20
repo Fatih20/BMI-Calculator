5107f25b1e78464d50b68492a7812ed071a67a25
Fatih20 <fnri39@protonmail.com>
2021-12-20 14:42:07 +0700

    Unrefined input

--- src/components/formInput.js
+++ src/components/formInput.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 const Main = styled.div`
+    align-items: center;
     display: flex;
-    justify-content: center;
+    gap: 5px;
+
+    & p {
+        color: white;
+    }
 `;
 
-function StatefulInput(){
-    const[value, setValue] = useState(null);
+const StyledInput = styled.input`
+    height: 100%;
+
+    &::-webkit-outer-spin-button, &::-webkit-inner-spin-button{
+        appearance: none;
+        margin: 0;
+    }
+`;
+
+function StatefulInput(setExternalValue){
+    const[value, setValue] = useState("");
 
     function onChange(e){
-        console.log(e.target.value);
+        console.log(e);
+        setExternalValue(e.target.value);
         setValue(e.target.value);
+        console.log(e.target.value);
     }
 
-    return (
+    return ({
         value,
         onChange
-    )
+    })
 }
 
-export default function FormInput ({isMetric, whatData}){
-    const inputProps = StatefulInput();
+export default function FormInput ({isMetric, whatData, setExternalValue}){
+    const inputProps = StatefulInput(setExternalValue);
 
     const unitConverter = {
         "true" : {
@@ -33,11 +49,20 @@ export default function FormInput ({isMetric, whatData}){
             "Weight" : "lbs",
         }
     }
+
+    function onlyNumberKey(evt) {
+        var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+        if (ASCIICode < 58 && ASCIICode > 47){
+            return false;
+        }
+        return true;
+    }
     
     return (
         <Main>
-            <label for={whatData}>{whatData}</label>
-            <input id={whatData} type="number" name={whatData} {...inputProps} />
+            <p>{whatData}</p>
+            <StyledInput id={whatData} type="number" name={whatData} {...inputProps} />
+            <p>{unitConverter[isMetric.toString()][whatData]}</p>
         </Main>
     )
 }
