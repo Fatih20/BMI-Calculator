fc29676d7e8d8a0ebe702065d741dfc5a7809e4c
Fatih20 <fnri39@protonmail.com>
2021-12-20 11:46:20 +0700

    Add global transition and vanilla button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import styled from "styled-components";
 
 import './App.css';
 
+import Header from "./components/header";
+import Body from "./components/body";
+
+import { GlobalTransition } from "./GlobalComponent";
+
 const Main = styled.div`
+  margin: 0;
+  padding: 0;
 `;
 
 function App() {
   return (
-    <Main>
-      
-    </Main>
+    <>
+      <GlobalTransition />
+      <Main>
+        <Header />
+        <Body />
+      </Main>
+    </>
   );
 }
 
