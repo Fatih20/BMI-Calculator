c89b5b5ed279b95789870833f948243d681a2e80
Fatih20 <fnri39@protonmail.com>
2021-12-20 11:46:49 +0700

    Initialization of charts

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,2 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+
+const Main = styled.div`
+`;
+
+
+export default function Charts (){
+    return (
+        <Main>
+            
+        </Main>
+    )
+}
